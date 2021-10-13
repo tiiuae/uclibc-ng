dc02dcb1bef7bf114180ab015d9943effd8336f3
Joonas Onatsu <joonas.onatsu@unikie.com>
2021-10-14 02:53:39 +0300

    Workaround for missing futimesat() implementation

--- libc/sysdeps/linux/common/futimesat.c
+++ libc/sysdeps/linux/common/futimesat.c
@@ -2,15 +2,32 @@
  * futimesat() for uClibc
  *
  * Copyright (C) 2009 Analog Devices Inc.
+ * Copyright (C) 2021 Technology Innovation Institute
  *
  * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
  */
 
 #include <sys/syscall.h>
 #include <sys/time.h>
+#include <sys/stat.h>
 
-#ifdef __NR_futimesat
-_syscall3(int, futimesat, int, fd, const char *, file, const struct timeval *, tvp)
-#else
-/* should add emulation with futimes() and /proc/self/fd/ ... */
+#if defined __NR_futimesat && defined __NR_utimensat
+/*
+ * Approach below modified from:
+ * libc/sysdeps/linux/common/futimens.c
+ * libc/sysdeps/linux/common/lutimes.c
+ */
+int futimesat (int fd, const char *file, const struct timeval tvp[2])
+{
+	struct timespec ts[2];
+
+	if (tvp != NULL)
+	{
+		TIMEVAL_TO_TIMESPEC(&tvp[0], &ts[0]);
+		TIMEVAL_TO_TIMESPEC(&tvp[1], &ts[1]);
+		return utimensat(fd, file, ts, 0);
+	}
+	__set_errno(EINVAL);
+	return -1;
+}
 #endif
